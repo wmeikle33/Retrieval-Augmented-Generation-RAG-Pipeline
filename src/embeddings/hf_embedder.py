c3fda1b3494b6572c0b346758bdafbd6848e02bd
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-12 18:47:05 +0800

    Update hf_embedder.py

--- src/embeddings/hf_embedder.py
+++ src/embeddings/hf_embedder.py
@@ -9,5 +9,5 @@ class HFEmbedder(BaseEmbedder):
     def embed_documents(self, texts):
         return self.model.encode(texts, normalize_embeddings=True)
 
-    def embed_query(self, text):
+    def embed(self, text):
         return self.embed_documents([text])[0]
