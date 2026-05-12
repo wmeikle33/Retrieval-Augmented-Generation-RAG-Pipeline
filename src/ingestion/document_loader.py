68a47b11bc4dfa82a2f65f23d5c3b4b6261adf28
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-12 17:22:26 +0800

    Update document_loader.py

--- src/ingestion/document_loader.py
+++ src/ingestion/document_loader.py
@@ -1,4 +1,5 @@
 class TextFolderLoader:
-    def __init__(self, docuemnt: str, encoding: str = "utf-8", pattern: str = "*.txt") -> None:
-        loader = PyPDFLoader(file_path)
-        return loader.load() 
+    def __init__(self, folder: str, encoding: str = "utf-8", pattern: str = "*.txt") -> None:
+        self.folder = Path(folder)
+        self.encoding = encoding
+        self.pattern = pattern
